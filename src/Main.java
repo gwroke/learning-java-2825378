b9688db864d725de5b8c09e6832bc10a0b519ce2
Woolley <george.woolley@roke.co.uk>
2025-10-16 14:42:59 +0100

    done

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
 public class Main {
 
-    public static void calculateTotalMealPrice(double listedMealPrice,
+    public static double calculateTotalMealPrice(double listedMealPrice,
                                                double tipRate,
                                                double taxRate) {
         double tip = tipRate * listedMealPrice;
         double tax = taxRate * listedMealPrice;
         double result = listedMealPrice + tip + tax;
-        System.out.println("Your total meal price is " + result);
+        return result;
+        
     }
 
     public static void main(String[] args) {
-        
+        double price = calculateTotalMealPrice(15, 0.5, 0.2);
+        System.out.println("Your total meal price is " + price);
     }
 
 }
