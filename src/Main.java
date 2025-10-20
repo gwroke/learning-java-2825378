3b05f79c0a13233ad4cc7b43cf2a875484da37df
Woolley <george.woolley@roke.co.uk>
2025-10-20 15:36:38 +0100

    donre

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
+        double result = Math.pow(2, 5);
+        System.out.println(result);
+
+        String s = "test";
+        char c = s.charAt(0);
+
+        StringBuilder t = new StringBuilder();
         
     }
 
