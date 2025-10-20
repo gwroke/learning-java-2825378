88d720e6916ed6f8671a5eaf87c29bae5efbd3bc
Woolley <george.woolley@roke.co.uk>
2025-10-20 15:41:23 +0100

    don

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        Triangle triA = new Triangle(15, 8, 15, 8, 17);
+        Triangle triB = new Triangle(3, 2.598, 3, 3, 3);
+        
     }
 
 }
